2e334d2e413e8a9eef8561594301af6bce2fac91
Lenny Lee <lennylee91@gmail.com>
2019-11-03 02:24:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
-# Make your shoe class here!
+class Shoe
+  
+  attr_accessor :color, :size, :material, :condition = 
+    def initialize(brand)
+    end
+    
+    def brand
+      "Nike"
+    end
+    
+    def color
+      "red"
+    end
+    
+    def cobble
+    puts "Your shoe is as good as new!"
+    
+   
+
+  end
+  
+
+end
